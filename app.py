f623c2e1436f4db528816d0b30cd53c4a497da5b
root <root@handsaw1.fyre.ibm.com>
2020-04-22 19:52:58 -0700

    Another route added

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def generate_buzz():
     page += '</h1></body></html>'
     return page
 
+@app.route("/name")
+def generate_buzz():
+    page = '<html><body><h1>'
+    page += Hi I Am Pavan.
+    page += '</h1></body></html>'
+    return page
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
